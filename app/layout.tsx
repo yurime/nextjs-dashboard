51a669676ccf2c42f5ea3c6ad146f205233629fd
Yuri Meshman <syurim@gmail.com>
2025-01-10 20:41:46 +0200

    lesson 9 commit
    - Implementing Partial Pre-Rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true;
 
 export default function RootLayout({
   children,
